3b12436e3207f51c555e15daddca7fd75043c50a
Singaram Senthil <ssingarammech@gmail.com>
2021-03-21 18:14:52 +0530

    fileupdated

---
+++ library.java
@@ -0,0 +1,60 @@
+public class Library{
+int annualFees=50;
+boolean lendbooks=true;
+float salary=18000.0f;
+
+
+public static void main(String[]args)
+{
+Library librarian = new Library();
+System.out.println(librarian.annualFees);
+System.out.println(librarian.lendbooks);
+System.out.println(librarian.salary);
+librarian.maintain_books();
+librarian.collect_fees();
+
+}
+public void maintain_books()
+{
+	//Method Body
+	System.out.println("arrange books");
+}
+public void collect_fees()
+{
+	if(annualFees!=50)
+	{
+	System.out.println("Fees  paid");
+	}
+	else
+	{
+		System.out.println("Not paid");
+		
+	}
+	
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
