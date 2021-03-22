adba38e5235b1c23458ea4c0c7e1dd88323a4297
Singaram Senthil <ssingarammech@gmail.com>
2021-03-22 14:52:19 +0530

    updatedfile

---
+++ CreateObjectExample.java
@@ -0,0 +1,11 @@
+public class CreateObjectExample1
+{
+public static void main(String[]args)
+{
+CreateObjectExample1 obj = new CreateObjectExample1();
+obj.show();
+}
+public void show()
+{
+System.out.println("WELCOME TO MY JAVA")
+}
