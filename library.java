1c1624d2ba6326c9926033b4ba64db963b7a7f25
Singaram Senthil <ssingarammech@gmail.com>
2021-03-22 16:04:37 +0530

    latest updae

--- library.java
+++ library.java
@@ -11,33 +11,11 @@ Library librarian = new Library();
 System.out.println(librarian.annualFees);
 System.out.println(librarian.lendbooks);
 System.out.println(librarian.salary);
-librarian.maintain_books();
-librarian.collect_fees();
-
-}
-public void maintain_books()
-{
-	//Method Body
-	System.out.println("arrange books");
-System.out.println("Maintining Books Area");
-}
-public void collect_fees()
-{
-	if(annualFees!=50)
-	{
-	System.out.println("Fees  paid");
-	}
-	else
-	{
-		System.out.println("Not paid");
-		
-}
-	
-}
-}
 
 
-no need to write maintain_books and collect_fees
+}
+
+}
 
 
 
